9ba3d624d0a8686bb71248a75bc6860762d8df22
Woosung Park <woosupar@c6r2s1.42seoul.kr>
2023-12-02 16:30:19 +0900

    2nd try first commit

--- get_next_line.c
+++ get_next_line.c
@@ -5,106 +5,57 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: woosupar <woosupar@student.42seoul.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/03 12:43:45 by woosupar          #+#    #+#             */
-/*   Updated: 2023/12/02 12:32:14 by woosupar         ###   ########.fr       */
+/*   Created: 2023/12/02 12:36:09 by woosupar          #+#    #+#             */
+/*   Updated: 2023/12/02 16:23:50 by woosupar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-ssize_t	read_value(char	**buf, int fd)
+ssize_t	fun_read(int fd, char **buf)
 {
-	ssize_t	val;
-
-	*buf = (char *) malloc (BUFFER_SIZE + 1);
+	ssize_t		val;
+	
+	*buf = (char *) malloc(BUFFER_SIZE + 1);
 	if (*buf == 0)
-		return (-1);
-	val = read(fd, *buf, BUFFER_SIZE);
+		return (0);
+	val = read(fd, &buf, BUFFER_SIZE);
 	if (val > 0)
 	{
-		(*buf)[val] = '\0';
+		*buf[val] = '\0';
 		return (val);
 	}
 	else if (val == 0)
-	{
-		*(buf)[val] = '\0';
 		return (0);
-	}
 	else
 		return (-1);
+	
 }
 
-char	*get_one_line(t_gnl *list, int fd, ssize_t i, char *ret)
+char	*get_one_line(int fd, t_gnl *gnl)
 {
-	char	*buf;
-	char	*temp;
+	char		*buf;
+	ssize_t		ret_fun_read;
 
 	buf = 0;
-	temp = (char *)malloc(1);
-	temp[0] = '\0';
-	while (read_value(&buf, fd) > 0)
+	ret_fun_read = fun_read(fd, &buf);
+	while (ret_fun_read > 0)
 	{
-		i = ft_strchr(buf);
-		if (i > 0 && i != BUFFER_SIZE)
-		{
-			ret = ft_strjoin(buf, temp, list, i + 1);
-			return (ret);
-		}
-		else if (i == BUFFER_SIZE)
-		{
-			temp = ft_strjoin(buf, temp, list, i);
-			buf = 0;
-		}
-	}
-	if (read_value(&buf, fd) == 0)
-	{
-		ret = ft_strjoin(buf, temp, list, i);
-		return (0);
-	}
-	return (ret);
-}
 
-t_gnl	*fd_check(t_gnl **line_list, int fd)
-{
-	t_gnl	*cur;
-
-	cur = *line_list;
-	if (line_list == 0 || *line_list == 0)
-		return (0);
-	while (cur != 0)
-	{
-		if (cur->fd == fd)
-			return (cur);
-		cur = cur->next;
 	}
-	return (0);
+	if (ret_Fun_read == 0)
+		return ;
 }
 
 char	*get_next_line(int fd)
 {
-	static t_gnl	**line_list;
-	t_gnl			*target_node;
-	ssize_t			i;
-	char			*del_target;
-	char			*ret;
+	char				*ret;
+	ssize_t				read_v;
+	static	t_gnl		gnl_array[49152];
 
-	i = 0;
-	ret = 0;
-	del_target = 0;
 	if (fd < 0)
 		return (0);
-	if (line_list == 0)
-		line_list = (t_gnl **) malloc(sizeof(t_gnl *));
-	*line_list = 0;
-	target_node = fd_check(line_list, fd);
-	if (target_node)
-		del_target = get_one_line(target_node, fd, i, ret);
-	else
-	{
-		target_node = make_node_add_back(line_list, fd);
-		del_target = get_one_line(target_node, fd, i, ret);
-	}
-	if (del_target == 0)
-		delete_target_node(line_list, target_node);
-	return (del_target);
+	ret = get_one_line(fd, &gnl_array[fd]);
+
+	return (ret);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: woosupar <woosupar@student.42seoul.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/01 15:34:29 by woosupar          #+#    #+#             */
-/*   Updated: 2023/12/02 11:54:02 by woosupar         ###   ########.fr       */
+/*   Updated: 2023/12/02 15:50:57 by woosupar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,28 +17,15 @@
 # include <stdlib.h>
 
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 6
+#  define BUFFER_SIZE 20
 # elif BUFFER_SIZE <= 0
 #  error "error"
 # endif
 
-typedef struct s_gnl
+typedef struct		s_gnl
 {
-	char			*remain;
-	ssize_t			remain_len;
-	ssize_t			buffer;
-	ssize_t			fd;
-	struct s_gnl	*next;
+	char			*rem;
+	ssize_t			len;
 }					t_gnl;
-
-ssize_t		read_value(char **buf, int fd);
-char		*get_one_line(t_gnl *list, int fd, ssize_t i, char *ret);
-t_gnl		*fd_check(t_gnl **line_list, int fd);
-char		*get_next_line(int fd);
-t_gnl		*make_node_add_back(t_gnl **line_list, int fd);
-void		delete_target_node(t_gnl **line_list, t_gnl *target_node);
-ssize_t		ft_strchr(char *buf);
-char		*ft_strjoin(char *buf, char *temp, t_gnl *list, ssize_t i);
-char		*move_remain(char *buf, ssize_t len, t_gnl *list);
 
 #endif

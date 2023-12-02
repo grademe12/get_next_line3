30df457179e773da7c3d415657b607d34ed8b852
Woosung Park <woosupar@c6r2s1.42seoul.kr>
2023-12-02 18:11:47 +0900

    12-02 02

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: woosupar <woosupar@student.42seoul.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/02 12:36:09 by woosupar          #+#    #+#             */
-/*   Updated: 2023/12/02 16:23:50 by woosupar         ###   ########.fr       */
+/*   Updated: 2023/12/02 18:11:13 by woosupar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,9 @@
 ssize_t	fun_read(int fd, char **buf)
 {
 	ssize_t		val;
+	ssize_t		i;
 	
+	i = 0;
 	*buf = (char *) malloc(BUFFER_SIZE + 1);
 	if (*buf == 0)
 		return (0);
@@ -23,36 +25,45 @@ ssize_t	fun_read(int fd, char **buf)
 	if (val > 0)
 	{
 		*buf[val] = '\0';
-		return (val);
+		while (*buf[i] != '\0')
+		{
+			if (*buf[i] == '\n')
+				return (i);
+			i++;
+		}
+		return (i);
 	}
 	else if (val == 0)
-		return (0);
-	else
 		return (-1);
+	else
+		return (-2);
 	
 }
 
 char	*get_one_line(int fd, t_gnl *gnl)
 {
 	char		*buf;
-	ssize_t		ret_fun_read;
+	ssize_t		i;
 
 	buf = 0;
-	ret_fun_read = fun_read(fd, &buf);
-	while (ret_fun_read > 0)
+	i = fun_read(fd, &buf);
+	while (i > 0)
 	{
-
+		if (i != BUFFER_SIZE)
+			return (ft_strjoin(buf, gnl);
+		else
+			ft_strjoin(buf, gnl);
 	}
-	if (ret_Fun_read == 0)
+	if (ret_fun_read == 0)
 		return ;
 }
 
 char	*get_next_line(int fd)
 {
 	char				*ret;
-	ssize_t				read_v;
 	static	t_gnl		gnl_array[49152];
 
+	i = 0;
 	if (fd < 0)
 		return (0);
 	ret = get_one_line(fd, &gnl_array[fd]);
